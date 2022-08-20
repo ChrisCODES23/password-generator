e5680632fff4d16e6406e7a708755dd839e8c16d
ChrisCODES23 <christiantalledo89@gmail.com>
2022-08-19 20:49:00 -0700

    I created a variable for uppercaseletters, lowercaseletters, numbers and specialcharacters

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,14 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+
+function generate(){
+  var lowerCaseLetters: ["abcdefghijklmnopqrstuvwxyz"];
+  var upperCaseLetters: ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+  var numbers : ["0123456789"];
+  var specialChar:[]
+}
 // ?WHEN prompted for the length of the password
 //  ?THEN I choose a length of at least 8 characters and no more than 128 characters
   passwordText.value = password;
@@ -19,7 +27,6 @@ WHEN I answer each prompt
 // ? THEN my input should be validated and at least one character type should be selected
 //  Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-if 
 
 // ? WHEN all prompts are answered
 // ? THEN a password is generated that matches the selected criteria
