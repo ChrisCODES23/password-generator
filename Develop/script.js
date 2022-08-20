f0e7616a676c7c6c1a9a337a459f62859ef27bc1
ChrisCODES23 <christiantalledo89@gmail.com>
2022-08-20 09:45:45 -0700

    added conditional statements for the user to choose a password that fits the criteria.

--- Develop/script.js
+++ Develop/script.js
@@ -9,42 +9,80 @@ var specialChar = ["!","\u0022","#","$","%","&","'","(",")","*","+",",","-",
 
 
 
+// ? variable to store the password generated
+var passwordProcessing = [];
 
-
-
-var superArray = [];
-
-
+// ? User Prompts 
 
 function generatePassword (){
 
-  var userLength = prompt {
+  var userLength = prompt (
     "How many characters do you want your password to have? \n Note: It must be between 8 and 128"
-  };
+  );
  
-  var userLowerCase = confirm {
+  var userLowerCase = confirm (
     "Do you want your password to have lowercase letters? \n Click ok if yes and click cancel if no"
-  };
+  );
 
-  var userUpperCase = confirm{
+  var userUpperCase = confirm(
     "Do you want your password to have uppercase (CAPITAL) letters? \n Click ok if yes and click cancel if no"
 
-  };
+  );
 
-  var userSpecialCharacters = confirm{
+  var userNumber = confirm (
+    "Do you want your password to have numbers? \n Click ok if yes and cancel if no"
+  )
+
+  var userSpecialCharacters = confirm(
     "Do you want your password to have special characters? \n Click ok if yes and click cancel if no"
-  };
-
+  );
+}
   
+// ? Conditional statements for the user to pick at least 1 character and a number between 8 and 128
 
 
+
+if  (userLength <8 || userLength > 128){
+  alert ("Error! please generate a number between 8 and 128")
+  return generatePassword();
+}
+if (!userLowerCase && !userSpecialCharacters, !userSpecialCharacters && !numbers){
+  alert("Error! please pick one category from the characters")
+ return generatePassword();
+}
+
+else(){
+  alert ("Please wait patiently your password is being made");
+}
+
+// ? If condition is met then the function will proceed
+// ? user selects any criteria to contain then a lowercase, uppercase ,numbers special characters
+
+if (userLowerCase){
+  for (var i = 0; i < userLowerCase.length; i++)
+  passwordProcessing.push(lowerCaseLetters[i]);
+}
+
+if (userUpperCase){
+  for (var i =0; i< userUpperCase.length; i++)
+  passwordProcessing.push(upperCaseLetters[i]);
+}
+
+if (userNumber){
+  for (var i= 0; i< userNumber.length; i++)
+  passwordProcessing.push(numbers[i]);
 }
 
 
 
+if (userSpecialCharacters){
+  for (var i=0; i< userSpecialCharacters.length; i++)
+  passwordProcessing.push(specialChar[i]);
+}
 
 
-
+// ?
+var password = [];
 
 
 
@@ -81,8 +119,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
 
-// ?WHEN prompted for the length of the password
-//  ?THEN I choose a length of at least 8 characters and no more than 128 characters
+
   passwordText.value = password;
   // ? WHEN asked for character types to include in the password
 //  ? THEN I [confirm] whether or not to include lowercase, uppercase, numeric, and/or special characters
@@ -99,6 +136,8 @@ function writePassword() {
 megaArray = []
 
 generateBtn.addEventListener("click", writePassword);
+generatePassword();
+console.log("You clicked it yay");
 
 // ? WHEN all prompts are answered
 // ? THEN a password is generated that matches the selected criteria
