0f3adb9ad5ac3f3f21c63cd26811496fdd469864
ChrisCODES23 <christiantalledo89@gmail.com>
2022-08-19 21:57:01 -0700

    I added prompts/confirms for the user

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,77 @@
 // Assignment Code
+// ? Created an array of letters numbers and special characters
+var lowerCaseLetters =  "abcdefghijklmnopqrstuvwxyz".split("");
+var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var numbers =  "0123456789".split("");
+var specialChar = ["!","\u0022","#","$","%","&","'","(",")","*","+",",","-",
+".","/",":",";","<","=",">","?","@","[","\u005C","]","^","_","`","{",
+"|", "}","~",];
+
+
+
+
+
+
+var superArray = [];
+
+
+
+function generatePassword (){
+
+  var userLength = prompt {
+    "How many characters do you want your password to have? \n Note: It must be between 8 and 128"
+  };
+ 
+  var userLowerCase = confirm {
+    "Do you want your password to have lowercase letters? \n Click ok if yes and click cancel if no"
+  };
+
+  var userUpperCase = confirm{
+    "Do you want your password to have uppercase (CAPITAL) letters? \n Click ok if yes and click cancel if no"
+
+  };
+
+  var userSpecialCharacters = confirm{
+    "Do you want your password to have special characters? \n Click ok if yes and click cancel if no"
+  };
+
+  
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 // ? WHEN I click the [button] to generate a password
 // ? THEN I am presented with a series of [prompts] for password criteria
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var generateBtn = document.querySelector("#generate");
 // ?WHEN[prompted] for password criteria
 // ? WHEN I select which criteria to include in the password
@@ -10,22 +81,23 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
 
-function generate(){
-  var lowerCaseLetters: ["abcdefghijklmnopqrstuvwxyz"];
-  var upperCaseLetters: ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-  var numbers : ["0123456789"];
-  var specialChar:[]
-}
 // ?WHEN prompted for the length of the password
 //  ?THEN I choose a length of at least 8 characters and no more than 128 characters
   passwordText.value = password;
   // ? WHEN asked for character types to include in the password
 //  ? THEN I [confirm] whether or not to include lowercase, uppercase, numeric, and/or special characters
 }
-WHEN I answer each prompt
+//? WHEN I answer each prompt
 // ? WHEN I answer each prompt
 // ? THEN my input should be validated and at least one character type should be selected
 //  Add event listener to generate button
+
+
+
+
+
+megaArray = []
+
 generateBtn.addEventListener("click", writePassword);
 
 // ? WHEN all prompts are answered
